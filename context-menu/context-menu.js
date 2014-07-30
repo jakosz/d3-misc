237b6b283fd9fbb956a6883ed49cc062eec967f4
Jacek Szejda <jakosz@gmail.com>
2014-07-30 21:40:42 +0200

    added several features:
    automatic box width
    click handling
    interaction handlers

--- context-menu/context-menu.js
+++ context-menu/context-menu.js
@@ -1,37 +1,87 @@
 function contextMenu() {
     var itemHeight = 20,
-        itemWidth = 100,
-        items = ['item'], 
-        itemStyle = {
-            'fill': 'rgb(244,244,244)', 
-            'stroke': 'white', 
-            'stroke-width': '1px'
-        };
+        itemWidth, 
+        itemMargin = 10, 
+        items = [], 
+        style = {
+            'rect': {
+                'mouseout': {
+                    'fill': 'rgb(244,244,244)', 
+                    'stroke': 'white', 
+                    'stroke-width': '1px'
+                }, 
+                'mouseover': {
+                    'fill': 'rgb(200,200,200)'
+                }
+            }, 
+            'text': {
+                'fill': 'steelblue', 
+                'font-size': '10'
+            }
+        }; 
     
     function menu(x, y) {
+        d3.select('.context-menu').remove();
+
+        // Set itemWidth automatically:
+        d3.select('svg').selectAll('tmp')
+            .data(items).enter()
+            .append('text')
+            .text(function(d){ return d; })
+            .style(style.text)
+            .attr('x', -1000)
+            .attr('y', -1000)
+            .attr('class', 'tmp');
+        var w = d3.selectAll('.tmp')[0]
+                  .map(function(x){ return x.getBBox().width; });
+        itemWidth = d3.max(w) + 2 * itemMargin;
+        d3.selectAll('.tmp').remove();
+
+        // Draw the menu
         d3.select('svg')
             .append('g').attr('class', 'context-menu')
             .selectAll('tmp')
-            .data(items).enter().append('g').attr('class', 'menu-entry');
+            .data(items).enter()
+            .append('g').attr('class', 'menu-entry')
+            .on('mouseover', function(){ 
+                d3.select(this).select('rect').style(style.rect.mouseover) })
+            .on('mouseout', function(){ 
+                d3.select(this).select('rect').style(style.rect.mouseout) });
+        
         d3.selectAll('.menu-entry')
             .append('rect')
             .attr('x', x)
             .attr('y', function(d, i){ return y + (i * itemHeight); })
             .attr('width', itemWidth)
             .attr('height', itemHeight)
-            .style(itemStyle);
+            .style(style.rect.mouseout);
+        
         d3.selectAll('.menu-entry')
             .append('text')
             .text(function(d){ return d; })
             .attr('x', x)
             .attr('y', function(d, i){ return y + (i * itemHeight); })
             .attr('dy', itemHeight * .8) // TODO
-            .attr('dx', itemHeight / 2);
+            .attr('dx', itemMargin)
+            .style(style.text);
+
+        // Other interactions
+        d3.select('body')
+            .on('click', function() {
+                d3.select('.context-menu').remove();
+            });
+
     }
     
     menu.items = function(e) {
         if (!arguments.length) return items;
         for (i in arguments) items.push(arguments[i]);
+        return menu;
+    }
+    
+    menu.width = function(x) {
+        if (!arguments.length) return itemWidth;
+        itemWidth = x;
         return menu;
     }
 
